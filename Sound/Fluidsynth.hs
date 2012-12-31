8fbadd09f057a0cb046a156c66982704e7225111
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-12-31 01:43:51 -0800

    Synth type and creation.

--- Sound/Fluidsynth.hs
+++ Sound/Fluidsynth.hs
@@ -4,10 +4,18 @@ import Foreign.ForeignPtr.Safe
 
 import Sound.Fluidsynth.Internal
 
-newtype Settings = Settings { unSettings :: ForeignPtr C'fluid_settings_t }
+newtype Settings = Settings (ForeignPtr C'fluid_settings_t)
+newtype Synth = Synth (ForeignPtr C'fluid_synth_t)
 
 newSettings :: IO Settings
 newSettings = do
     ptr <- c'new_fluid_settings
     settings <- newForeignPtr p'delete_fluid_settings ptr
     return $! Settings settings
+
+newSynth :: Settings -> IO Synth
+newSynth (Settings settings) = do
+    withForeignPtr settings $ \ptr -> do
+        ptr' <- c'new_fluid_synth ptr
+        synth <- newForeignPtr p'delete_fluid_synth ptr'
+        return $! Synth synth
